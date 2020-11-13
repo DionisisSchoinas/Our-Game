c807a2029e3256c5acf184d672faa80966212217
DionisisSchoinas <p17134@students.cs.unipi.gr>
2020-11-13 22:04:37 +0200

    Added EnemyFireray

--- Assets/Scripts/Spells/Enemy/EnemyFireray.cs
+++ Assets/Scripts/Spells/Enemy/EnemyFireray.cs
@@ -2,17 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFireray : MonoBehaviour
+public class EnemyFireray : EnemySpell
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float damage = 5f;
+    [SerializeField]
+    private int damageTicksPerSecond = 5;
+
+    private List<GameObject> collisions;
+    private int damageablesLayer;
+
+    private GameObject tmpLaser;
+
+    private void Start()
     {
-        
+        collisions = new List<GameObject>();
+        damageablesLayer = LayerMask.NameToLayer("Damageables");
+        InvokeRepeating(nameof(DamageEnemies), 0f, 1f / damageTicksPerSecond);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public override void FireHold(bool holding, Transform firePoint)
     {
-        
+        if (holding)
+        {
+            tmpLaser = Instantiate(gameObject, firePoint);
+            tmpLaser.SetActive(true);
+        }
+        else
+        {
+            Destroy(tmpLaser);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer.Equals(damageablesLayer))
+        {
+            if (!collisions.Contains(other.gameObject))
+            {
+                if (LineCasting.isLineClear(other.transform.position, transform.position, damageablesLayer))
+                {
+                    collisions.Add(other.gameObject);
+                }
+            }
+        }
+    }
+
+    private void DamageEnemies()
+    {
+        foreach (GameObject gm in collisions)
+        {
+            if (gm != null)
+                HealthEventSystem.current.TakeDamage(gm.name, damage);
+        }
+        collisions.Clear();
+    }
+
+    public override void WakeUp()
+    {
+    }
+
+    public override void FireSimple(Transform firePoint)
+    {
     }
 }
