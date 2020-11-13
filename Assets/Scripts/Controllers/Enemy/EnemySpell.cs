6b54a42b5d5098964b24ed155ccb1e33cca8a986
DionisisSchoinas <p17134@students.cs.unipi.gr>
2020-11-13 22:00:09 +0200

    Restructured files / added enemy
    - EnemyWand
    - EnemySpell
    - EnemyFirebolt

---
+++ Assets/Scripts/Controllers/Enemy/EnemySpell.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class EnemySpell : MonoBehaviour
+{
+    abstract public void FireSimple(Transform firePoint);
+    abstract public void FireHold(bool holding, Transform firePoint);
+    abstract public void WakeUp();
+}
---
+++ Assets/Scripts/Controllers/Enemy/EnemyWand.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWand : MonoBehaviour
+{
+    [SerializeField]
+    private Transform simpleFirePoint;
+    [SerializeField]
+    private Transform channelingFirePoint;
+    [SerializeField]
+    private EnemySpell[] spells;
+
+    public bool fireSimple;
+    public bool fireChannel;
+
+    private bool hold;
+
+    private int selectedSpell;
+
+    
+    private void Start()
+    {
+        foreach (EnemySpell s in spells)
+        {
+            s.WakeUp();
+        }
+        if (spells.Length != 0) selectedSpell = 0;
+        hold = false;
+
+        InvokeRepeating(nameof(FireSpell), 0f, 2f);
+    }
+
+    public void SetSelectedSpell(int value)
+    {
+        selectedSpell = value;
+    }
+
+    public void Fire1()
+    {
+        spells[selectedSpell].FireSimple(simpleFirePoint);
+    }
+
+    public void Fire2(bool holding)
+    {
+        spells[selectedSpell].FireHold(!hold, channelingFirePoint);
+        hold = !holding;
+    }
+
+    private void FireSpell()
+    {
+        if (fireSimple) Fire1();
+        else Fire2(hold);
+    }
+}
