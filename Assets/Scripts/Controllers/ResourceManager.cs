6bf9b0f9a7b9a75d457de2398a24547554342a21
DionisisSchoinas <p17134@students.cs.unipi.gr>
2020-11-17 23:07:20 +0200

    Added basic effects for Burning and Electrified

--- Assets/Scripts/Controllers/ResourceManager.cs
+++ Assets/Scripts/Controllers/ResourceManager.cs
@@ -9,4 +9,10 @@ public class ResourceManager
         public static ParticleSystem Smoke = ((GameObject)Resources.Load("Spells/Default Smoke Source", typeof(GameObject))).GetComponent<ParticleSystem>();
         public static ParticleSystem Earth = ((GameObject)Resources.Load("Spells/Default Earth Source", typeof(GameObject))).GetComponent<ParticleSystem>();
     }
+
+    public class Effects
+    {
+        public static ParticleSystem Burning = ((GameObject)Resources.Load("Effects/Burning Effect", typeof(GameObject))).GetComponent<ParticleSystem>();
+        public static ParticleSystem Electrified = ((GameObject)Resources.Load("Effects/Electrified Effect", typeof(GameObject))).GetComponent<ParticleSystem>();
+    }
 }
