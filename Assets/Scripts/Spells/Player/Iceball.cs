510c82a4b69ef76ccd352fe6f49ceb025d737c6c
DionisisSchoinas <p17134@students.cs.unipi.gr>
2020-11-21 00:09:20 +0200

    Added Iceball
    - Added shader for ball of ice
    - Added 2 materials for the ball of ice and the explosion

---
+++ Assets/Scripts/Spells/Player/Iceball.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Iceball : Spell
+{
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private GameObject explosion;
+    [SerializeField]
+    private Rigidbody rb;
+
+    private SpellIndicatorController indicatorController;
+
+    public override void FireSimple(Transform firePoint)
+    {
+        GameObject tmp = Instantiate(gameObject, firePoint.position, firePoint.rotation) as GameObject;
+        Destroy(tmp, 5f);
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * speed * Time.deltaTime, ForceMode.VelocityChange);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject exp = Instantiate(explosion, transform.position + transform.forward * 0.2f, transform.rotation) as GameObject;
+        Destroy(exp, 3f);
+        Destroy(gameObject);
+    }
+    public override ParticleSystem GetSource()
+    {
+        return ResourceManager.Default.Ice;
+    }
+
+    public override void SetIndicatorController(SpellIndicatorController controller)
+    {
+        indicatorController = controller;
+    }
+
+    public override void FireHold(bool holding, Transform firePoint)
+    {
+    }
+    public override void WakeUp()
+    {
+    }
+}
