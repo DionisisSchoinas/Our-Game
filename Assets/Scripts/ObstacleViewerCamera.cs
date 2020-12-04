b14a396e614c53e8180175e45dcd2682b5039e41
DionisisSchoinas <p17134@students.cs.unipi.gr>
2020-12-04 19:12:07 +0200

    Added See through walls and Dodge
    - Some what working see thorugh Shaders
        -  Hole Marker Shader ( hole to be cut )
        -  Obstacle Hole Shader ( anything you want to be cuttable)
    - Added basic Dodge spell particles

---
+++ Assets/Scripts/ObstacleViewerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleViewerCamera : MonoBehaviour
+{
+    public Camera mainCam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GetComponent<Camera>();
+    }
+
+    private void FixedUpdate()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+        }
+    }
+}
