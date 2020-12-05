974b575fc0cfbb4e22f9b40a1553f26a7ee22c6c
DionisisSchoinas <p17134@students.cs.unipi.gr>
2020-12-06 00:06:34 +0200

    Added Fire effects and Swing
    - Added a simple swor with a simpel swing with Space
    - Added 2 fire swing effects

---
+++ Assets/- Testing Components/SwingController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingController : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();    
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("Swing", true);
+        }
+        else
+        {
+            animator.SetBool("Swing", false);
+        }
+    }
+}
