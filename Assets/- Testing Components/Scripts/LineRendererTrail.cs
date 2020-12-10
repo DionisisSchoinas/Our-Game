9fe12eed4ec1625f1b9c1a90b15cc64e774059fd
DionisisSchoinas <p17134@students.cs.unipi.gr>
2020-12-11 00:45:14 +0200

    Added new pack, beta version of line renderer swing trail

---
+++ Assets/- Testing Components/Scripts/LineRendererTrail.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererTrail : MonoBehaviour
+{
+    public int numberOfPointsSaved = 7;
+    public Transform tipPoint;
+    public Transform basePoint;
+    [Range(0,1)]
+    public float lineTolerance = 0;
+    public Gradient lineGradient;
+    public AnimationCurve lineCurve;
+    public List<Material> lineMaterials;
+
+    private LineRenderer line; 
+    private List<Vector3> points;
+    private bool allow;
+
+    void Awake()
+    {
+        points = new List<Vector3>();
+        allow = false;
+        // Instantiate line
+        line = gameObject.AddComponent<LineRenderer>();
+        // Lighting
+        line.receiveShadows = false;
+        line.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        // Poistions
+        line.positionCount = 0;
+        // Color
+        line.colorGradient = lineGradient;
+        // Curve
+        AnimationCurve newCurve = new AnimationCurve();
+        float mag = (tipPoint.position - basePoint.position).magnitude;
+        foreach (Keyframe key in lineCurve.keys)
+        {
+            newCurve.AddKey(key.time, key.value * mag);
+        }
+        line.widthCurve = newCurve;
+        // Materials
+        line.materials = lineMaterials.ToArray();
+    }
+
+    void FixedUpdate()
+    {
+        if (allow)
+        {
+            if (points.Count == numberOfPointsSaved) points.RemoveAt(numberOfPointsSaved - 1);
+            points.Insert(0, SpawnPoint());
+
+            line.positionCount = points.Count;
+            line.SetPositions(points.ToArray());
+        }
+    }
+
+    public void StopLine()
+    {
+        allow = false;
+        line.positionCount = 0;
+        points.Clear();
+    }
+
+    public void StartLine()
+    {
+        allow = true;
+    }
+
+    private Vector3 SpawnPoint()
+    {
+        return Vector3.Lerp(basePoint.position, tipPoint.position, 0.5f);
+    }
+}
--- Assets/- Testing Components/SwingController.cs
+++ Assets/- Testing Components/SwingController.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 public class SwingController : MonoBehaviour
 {
     Animator animator;
+    bool swing;
+    
+    public ParticleSystem trail;
+    public float charge;
+    public float swinging;
+    public float reset;
+
+    bool allow;
+
+    Coroutine coroutine;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        allow = true;
+        swing = false;
+        //trail.Stop();
+    }
+
+    private void Update()
+    {
+        if (allow && Input.GetKeyDown(KeyCode.Space))
+        {
+            swing = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            swing = false;
+        }
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (swing)
         {
-            animator.SetBool("Swing", true);
-        }
-        else
-        {
-            animator.SetBool("Swing", false);
+            swing = false;
+            Swing();
         }
 
         if (Input.GetKey(KeyCode.Mouse1))
@@ -31,5 +53,30 @@ public class SwingController : MonoBehaviour
     private void Fire()
     {
 
+    }
+
+    private void Swing()
+    {
+        allow = false;
+
+        //if (coroutine != null) StopCoroutine(coroutine);
+        coroutine = StartCoroutine(StartSwing());
+    }
+
+    IEnumerator StartSwing()
+    {
+        animator.SetBool("Swing", true);
+        yield return new WaitForSeconds(charge);
+        //trail.Play();
+        StartCoroutine(StopSwing());
+    }
+
+    IEnumerator StopSwing()
+    {
+        yield return new WaitForSeconds(swinging);
+        //trail.Stop();
+        yield return new WaitForSeconds(reset);
+        animator.SetBool("Swing", false);
+        allow = true;
     }
 }
