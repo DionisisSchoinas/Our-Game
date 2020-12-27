9912f4a9e2b6952b65529543ac4d28c55f0592e4
DionisisSchoinas <p17134@students.cs.unipi.gr>
2020-12-27 21:17:48 +0200

    Added Warrior
    - Animations
    - Combos
    - New controllers

---
+++ Assets/Scripts/Controllers/Player/Combo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combo : MonoBehaviour
+{
+    public float delayCombo1 ;
+    public float delayCombo2;
+    public float delayCombo3;
+}
