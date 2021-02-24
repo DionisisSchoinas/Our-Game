ca3dfcbd54be71e1ce8536ffd4af1bbcde1ad2a2
Schoinas Dionisis <schinasdionisis@gmail.com>
2021-02-24 17:49:48 +0200

    Start Menu added

---
+++ Assets/Scripts/Controllers/UI/CharacterSelectScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectScript : MonoBehaviour
+{
+    public CanvasGroup startMenu;
+
+    private CanvasGroup canvasGroup;
+    private Button wizard;
+    private Button fighter;
+    private Button backToMenu;
+
+    private void Awake()
+    {
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        OverlayControls.SetCanvasState(false, canvasGroup);
+
+        Button[] buttons = gameObject.GetComponentsInChildren<Button>();
+        wizard = buttons[0];
+        fighter = buttons[1];
+        backToMenu = buttons[2];
+
+        wizard.onClick.AddListener(WizardPick);
+        fighter.onClick.AddListener(FighterPick);
+        backToMenu.onClick.AddListener(BackToMenu);
+    }
+
+    private void WizardPick()
+    {
+
+    }
+
+    private void FighterPick()
+    {
+
+    }
+
+    private void BackToMenu()
+    {
+        OverlayControls.SetCanvasState(true, startMenu);
+        OverlayControls.SetCanvasState(false, canvasGroup);
+    }
+}
---
+++ Assets/Scripts/Controllers/UI/StartMenuScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenuScript : MonoBehaviour
+{
+    public CanvasGroup characterSelect;
+
+    private CanvasGroup canvasGroup;
+    private Button startButton;
+    private Button exitButton;
+
+    private int mode;
+
+    private void Awake()
+    {
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        OverlayControls.SetCanvasState(true, canvasGroup);
+
+        Button[] buttons = gameObject.GetComponentsInChildren<Button>();
+        startButton = buttons[0];
+        exitButton = buttons[1];
+
+        startButton.onClick.AddListener(StartGameClick);
+        exitButton.onClick.AddListener(ExitButtonClick);
+
+        mode = -1;
+
+        YesNoDialog.current.onResponded += Response;
+    }
+
+    private void OnDestroy()
+    {
+        YesNoDialog.current.onResponded -= Response;
+    }
+
+    private void StartGameClick()
+    {
+        OverlayControls.SetCanvasState(true, characterSelect);
+        OverlayControls.SetCanvasState(false, canvasGroup);
+    }
+
+    private void ExitButtonClick()
+    {
+        mode = 0;
+        OverlayControls.SetCanvasState(0.2f, canvasGroup);
+        YesNoDialog.SetDialogText("Exit the Game");
+    }
+
+    private void Response(bool response)
+    {
+        if (response)
+        {
+            if (mode == 0)
+            {
+                ExitGame();
+            }
+        }
+        OverlayControls.SetCanvasState(true, canvasGroup);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
